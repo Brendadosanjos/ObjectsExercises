6439665854f270654532d8d53d348d512d1799f9
brendadosanjos <brendakelly_oliveira@live.com>
2024-09-17 18:54:42 -0300

    inicio dos exercicios

--- exercises.js
+++ exercises.js
@@ -11,30 +11,87 @@ let pessoas = [
     console.log(pessoas[u].nome);  
   }
 
+
+
+
 /*17.Percorrer Propriedades de um Objeto
 Escreva um loop que percorra um objeto e imprima todas as suas chaves e valores.*/
+let obj = {a: 1, b: 2, c: 3};
+
+for (let key in obj) {
+  console.log(key); // 'a', 'b', 'c' (chaves)
+  console.log(obj[key]); // 1, 2, 3 (valores)
+}
+
+
 
 
 /*18.Cópia de Elementos de um Array
 Escreva um loop que copie todos os elementos de um array para outro array.*/
+let arrayFrutas = ['maracuja', 'caja', 'caju', 'melão']
+let loop =[]
+
+for (let elemento of arrayFrutas){
+  loop.push(elemento)
+}
+console.log(loop);
+
+
+
+
+//ou com o método SPLICE
+let arraycopia = arrayFrutas.slice();
+console.log(arrayFrutas);
+
+
 
 
 /*19.Verificação de Presença de Número em um Array
 Escreva um loop que verifique se um número específico está presente em um array.*/
+let numeros = [5, 4, 4, 3, 2, 1]
+console.log(numeros.some(num => num === 4));
+
+
 
 
 /*20.Remoção de Elementos Duplicados de um Array
 Escreva um loop que remova todos os elementos duplicados de um array.*/
+let arrayOriginal = [1, 2, 3, 4, 3, 2, 5, 6, 1];
+let arraySemDuplicatas = [...new Set(arrayOriginal)];
+
+console.log(arraySemDuplicatas)
+
 
 
 /*21.Soma de Números Ímpares Múltiplos de Três
 Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se encontram no conjunto dos números de 1 até 500.*/
+let soma = 0;
+
+for (let i = 1; i <= 500; i++) {
+  if (i % 2 === 1 && i % 3 === 0) {
+    soma += i;
+  }
+}
+
+console.log(soma)
+
+
 
 
 /*22.Alturas de 15 Pessoas
 Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e mostrar:
 A menor altura do grupo;
 A maior altura do grupo;*/
+let alturas = [1.75, 1.82, 1.68, 1.90, 1.77, 1.65, 1.85, 1.80, 1.70, 1.72, 1.78, 1.88, 1.74, 1.76, 1.81];
+
+// Calcular a menor e a maior altura
+let menorAltura = Math.min(...alturas);
+let maiorAltura = Math.max(...alturas);
+
+console.log(`A menor altura do grupo é: ${menorAltura}`);
+console.log(`A maior altura do grupo é: ${maiorAltura}`);
+
+
 
 
 /*23.Média Aritmética e Percentual de Valores
